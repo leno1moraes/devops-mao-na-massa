9d8dc0b4c8b0086e0ef3e9da8cf178ac3d8338a3
3012056 <leno.moraes@ifmt.edu.br>
2023-05-05 21:24:19 -0400

    subir docker lab

--- docker-lab/provision.sh
+++ docker-lab/provision.sh
@@ -4,3 +4,8 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
+cat <<EOT >> /etc/hosts
+192.168.1.2 docker-lab
+EOT
+# cp /vagrant/arquivos/Dockerfile /root/ 
+# cp /vagrant/arquivos/easy-notes.jar /root/
